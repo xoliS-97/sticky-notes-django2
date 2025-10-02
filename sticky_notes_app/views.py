940680736acae883d0ac974f28f762563452b4d1
Xoli Sibiya” <Xoli.sibiya97@gmail.com>
2025-10-02 21:29:59 +0200

    Added edit/delete functionality and Bootstrap styling

--- sticky_notes_app/views.py
+++ sticky_notes_app/views.py
@@ -15,3 +15,21 @@ def add_note(request):
     else:
         form = NoteForm()
     return render(request, 'sticky_notes_app/add_note.html', {'form': form})
+
+from django.shortcuts import get_object_or_404
+
+def edit_note(request, note_id):
+    note = get_object_or_404(Note, id=note_id)
+    if request.method == 'POST':
+        form = NoteForm(request.POST, instance=note)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = NoteForm(instance=note)
+    return render(request, 'sticky_notes_app/edit_note.html', {'form': form})
+
+def delete_note(request, note_id):
+    note = get_object_or_404(Note, id=note_id)
+    note.delete()
+    return redirect('home')
